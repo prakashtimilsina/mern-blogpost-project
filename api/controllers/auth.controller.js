7342099f8a68a57f390aba39be360181bc5f20b0
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-09 13:55:18 -0600

    create sign in api route added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import {errorHandler} from '../utils/error.js'
+import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
     const { username, email, password} = req.body;
     
@@ -21,3 +22,32 @@ export const signup = async (req, res, next) => {
         next(error)
      }
 };
+
+
+export const signin = async(req, res, next)=>{
+   const {email, password} = req.body;
+   if(!email || !password || email ==='' || password === ''){
+      next(errorHandler(400, 'All fields are required'))
+   }
+try {
+   const validUser = await User.findOne({email});
+   if (!validUser){
+      return next(errorHandler(404, 'email/passsword is not valid'));
+   }
+   const validPassword = bcryptjs.compareSync(password, validUser.password);
+   if(!validPassword){
+      return next(errorHandler(400, 'email/passsword is not valid'))
+   }
+   const token = jwt.sign({id: validUser._id}, process.env.TOKEN_SECRET_KEY, {expiresIn: '1d'});
+   //removing password from user data.
+   const filterUser = await User.findById(validUser._id).select("-password");
+   res.status(200).cookie('access_token', token, {
+      httpOnly: true
+   }).json({user: filterUser, message: 'Signin Successful'});
+
+} catch (error) {
+   next(error)
+}
+
+}
+
