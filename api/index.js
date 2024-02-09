20d231bd8d6651be1a2638fdc2dc75b2a92bfe66
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-08 20:17:45 -0600

    MongoDB Connection Added

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URL)
+        .then(()=>{console.log("MongoDB is successfully connected")})
+        .catch((err)=>{
+            console.log("Error while connecting MongoDb and Error is : ", err.message)
+        })
 
 const app = express();
 
