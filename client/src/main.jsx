ef58ea849a274c67d652866c7d3d0ffe1628983c
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-17 10:32:53 -0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import {store} from './redux/store/store.js';
 import {Provider} from 'react-redux';
 
+//Removed Redux and will apply contextAPI
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
