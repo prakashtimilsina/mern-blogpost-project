0811c7a2be22a475e957b47021b0a1f415ef6e69
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-02-17 19:37:07 -0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {store} from './redux/store/store.js';
-import {Provider} from 'react-redux';
+// import {store} from './redux/store/store.js';
+// import {Provider} from 'react-redux';
 
 //Removed Redux and will apply contextAPI
 ReactDOM.createRoot(document.getElementById('root')).render(
