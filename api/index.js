7536f72fe9555b01a757ab04fba65db8adc73758
prakashtimilsina <80721127+prakashtimilsina@users.noreply.github.com>
2024-03-19 22:23:20 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000.')
 })
-
+//Routes
 app.use('/api/user', userRoutes);
 app.use('/api/auth/', authRoutes)
 
